bf59240560d5fa7613ba0511ad5e82f6ac4468dc
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-21 23:33:55 +0600

    add book

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import BooksView from '../components/Books/BooksView';
+import AddBook from '../components/Books/AddBook';
 import Footer from '../components/Shared/Footer';
 import Navbar from '../components/Shared/Navbar';
 import ErrorPage from '../pages/Error';
@@ -14,6 +15,7 @@ const PageIndex = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/show-books" element={<BooksView />} />
+          <Route path="/add-book" element={<AddBook />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </main>
